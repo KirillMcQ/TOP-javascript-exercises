9899b0ffcf30eec06b35a6eeec2b92b8b9c211bd
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-17 11:54:03 -0400

    Finished 8th problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let sum = 0;
+  nums.forEach((num) => sum+=num);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums === undefined || nums.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  nums.forEach((num) => sum+=num);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  let product = 1;
+  nums.forEach((num) => product*=num);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for (let i = 1; i <= num; ++i){
+    product*=i;
+  }
+  return product;
 };
 
 // Do not edit below this line
