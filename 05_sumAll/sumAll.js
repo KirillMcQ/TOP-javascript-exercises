df1fbad27ffc7d891e75ea113c01391eb583ab96
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-17 11:22:37 -0400

    Finished the 5th problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, lastInt) {
+    if (!Number.isInteger(firstInt) || ! Number.isInteger(lastInt) || firstInt < 0 || lastInt < 0) return "ERROR";
+    if (firstInt > lastInt) {
+        let temp = firstInt;
+        firstInt = lastInt;
+        lastInt = temp;
+    }
+    let sum = 0;
+    for (let i = firstInt; i <= lastInt; ++i) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
