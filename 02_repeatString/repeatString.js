476d32b0b2398517a86209b10d2a1c3a1751c856
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-17 11:09:29 -0400

    Completed first three problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, timeRepeated) {
+    let outputStr = "";
+    for (let i = 0; i < timeRepeated; ++i) {
+        outputStr+=str;
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let charArray = [];
+    for (const character of str) {
+        charArray.push(character);
+    }
+    charArray.reverse();
+    return charArray.join('');
 };
+
+reverseString('Hello! World!')
 
 // Do not edit below this line
 module.exports = reverseString;
