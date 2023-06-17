d0318c8855e3338c21dbc7622b81fca54a7b4527
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-17 11:15:03 -0400

    Finished the 4th problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...numsToRemove) {
+    let filteredArr = [];
+    for (let el of arr) {
+        if (!numsToRemove.includes(el)) {
+            filteredArr.push(el);
+        }
+    }
+    return filteredArr;
 };
 
 // Do not edit below this line
