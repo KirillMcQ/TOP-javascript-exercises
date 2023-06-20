6351f8ebf8ee20e5cd79489ad05d30bfb87cd252
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-20 16:14:00 -0400

    Finished excersize 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  // Found this regex on SO, I don't know how else to remove puncuation.
+  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  str = str.replace(/\s/g, "");
+  let reversedStr = str.split("").reverse().join("");
+  return str === reversedStr;
 };
 
 // Do not edit below this line
