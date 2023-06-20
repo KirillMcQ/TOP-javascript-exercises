ade6b1298219c55bcf4e1b338b7db7151b5bd505
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-20 17:00:52 -0400

    Finished final excersise 12, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let agesAndNames = [];
+  for (let person of arr) {
+    let currentAge;
+    if (person["yearOfDeath"] === undefined) {
+      currentAge = 2023 - person["yearOfBirth"];
+    } else {
+      currentAge = person["yearOfDeath"] - person["yearOfBirth"];
+    }
+    agesAndNames.push({ name: person["name"], age: currentAge });
+  }
+  let oldest = { age: 0 };
+  for (let curPerson of agesAndNames) {
+    if (curPerson["age"] > oldest["age"]) {
+      oldest = curPerson;
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
